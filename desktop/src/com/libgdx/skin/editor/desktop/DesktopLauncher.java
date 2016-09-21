31416044123777aaf4917ace5c285eddc07442aa
Mitkey <mitkey@foxmail.com>
2016-09-21 14:53:30 +0800

    init project
    Signed-off-by: Mitkey <mitkey@foxmail.com>

---
+++ desktop/src/com/libgdx/skin/editor/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.libgdx.skin.editor.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.libgdx.skin.editor.EditorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EditorGame(), config);
+	}
+}
