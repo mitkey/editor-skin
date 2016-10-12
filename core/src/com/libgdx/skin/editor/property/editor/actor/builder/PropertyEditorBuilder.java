8d3bb25205d7efeb15ebe21299c43184d028959d
Mitkey <mitkey@foxmail.com>
2016-10-12 16:12:22 +0800

    add com/libgdx/skin/editor/property/editor/actor/builder/PropertyEditorBuilder.java FloatBuilder editor dialog
    Signed-off-by: Mitkey <mitkey@foxmail.com>

--- core/src/com/libgdx/skin/editor/property/editor/actor/builder/PropertyEditorBuilder.java
+++ core/src/com/libgdx/skin/editor/property/editor/actor/builder/PropertyEditorBuilder.java
@@ -1,5 +1,6 @@
 package com.libgdx.skin.editor.property.editor.actor.builder;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
@@ -7,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton.ImageTextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
@@ -14,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.List.ListStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane.ScrollPaneStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldFilter;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
@@ -24,7 +27,10 @@ import com.badlogic.gdx.utils.reflect.Field;
 import com.badlogic.gdx.utils.reflect.ReflectionException;
 import com.libgdx.skin.editor.GlobalData;
 import com.libgdx.skin.editor.dialog.picker.BitmapFontPicker;
+import com.libgdx.skin.editor.utils.common.StrUtil;
 import com.libgdx.skin.editor.utils.scene2d.CustomSkin;
+import com.libgdx.skin.editor.utils.scene2d.Dialogs;
+import com.libgdx.skin.editor.utils.scene2d.Dialogs.InputDialogListener;
 
 /**
  * @作者 Mitkey
@@ -212,8 +218,30 @@ public abstract class PropertyEditorBuilder {
 		public Actor buildActor(CustomSkin projectSkin, Object styleObject, Field field, OnEditorCall onEditorCall) throws ReflectionException {
 			float value = (float) field.get(styleObject);
 			ImageTextButton imageTextButton = generationImageTextButton(String.valueOf(value), true);;
-
-			// TODO use onEditorCall
+			imageTextButton.addListener(new ChangeListener() {
+				@Override
+				public void changed(ChangeEvent event, Actor actor) {
+					Stage stage = imageTextButton.getStage();
+					String title = "Change Float Value";
+					String fieldName = "Float Value:";
+					String fieldValue = String.valueOf(value);
+					TextFieldFilter textFieldFilter = new Dialogs.InputDialogFloatFilter();
+					InputDialogListener inputDialogListener = new InputDialogListener() {
+						@Override
+						public void finished(String input) {
+							float inputFloat = StrUtil.isNotBlank(input) ? Float.valueOf(value) : 0;
+							try {
+								field.set(styleObject, inputFloat);
+							} catch (ReflectionException e) {
+								Gdx.app.error("Change Float Dialog", "change float error", e);
+								Dialogs.showOkDialog(stage, title + " Result", "error", GlobalData.skin);
+							}
+							onEditorCall.call();
+						}
+					};
+					Dialogs.showInputDialog(stage, title, fieldName, fieldValue, projectSkin, textFieldFilter, inputDialogListener);
+				}
+			});
 			return imageTextButton;
 		}
 	}
